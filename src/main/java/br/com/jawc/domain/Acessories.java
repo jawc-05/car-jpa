d928f8c41907aee7c8ee793eaa38ca8d6a0f2806
jawc-05 <joaoalfredowc@gmail.com>
2026-04-23 15:40:42 +0200

    feat: adding Accessories in domain

---
+++ src/main/java/br/com/jawc/domain/Acessories.java
@@ -0,0 +1,48 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_accesories")
+public class Acessories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accesories_seq")
+    @SequenceGenerator(name="accesories_seq", sequenceName = "seq_accesories", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @ManyToMany(mappedBy = "accesories")
+    private List<Car> cars;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
